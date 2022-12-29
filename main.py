6ee89afeb90c0eb16b49826d21be69d481c3b978
Herisson Hyan <herissonhyan@gmail.com>
2022-12-29 15:48:11 -0300

    Criação do banco de dados e suas funções

--- main.py
+++ main.py
@@ -1,20 +1,57 @@
+from io import BytesIO
+
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 from readModel import modelTF
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+db = SQLAlchemy(app)
+
+class Upload(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(50))
+    data = db.Column(db.LargeBinary)
+
+def criartabela():
+    db.create_all()
+    pass
+
+def addmodel(nome,patch):
+    def convert_into_binary(file_path):
+        with open(file_path, 'rb') as file:
+            binary = file.read()
+        return binary
+    data=convert_into_binary(patch)
+    upload = Upload(filename=nome, data=data)
+    db.session.add(upload)
+    db.session.commit()
+    pass
+
+def criarh5(binary_data, file_name):
+  with open(file_name, 'wb') as file:
+    file.write(binary_data)
 
 @app.route("/", methods=["GET"])
 def index():
-    #a=modelTF.readModel("images\passaro.jpg","modelo.h5")
+    #criartabela()
+    #addmodel("modelo","modelo.h5")
+    #upload = Upload.query.filter_by(id=1).first()
+    #criarh5(upload.data,"criado.h5")
+    #print(BytesIO(upload.data))
+
     return render_template('index.html')
 
 @app.route("/", methods=["POST"])
 def post_file():
     arquivo=request.files.get("minhaImage")
+    modelo=request.files.get("minhaimagem")
     path="images/"+arquivo.filename
     arquivo.save(path)
-    a=modelTF.readModel(path,"modelo.h5")
+    upload = Upload.query.filter_by(id=1).first()
+    a=modelTF.readModel(path)
     return a[0]
 
 
